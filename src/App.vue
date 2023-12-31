c0b429a842affdd869b4c3497d878df8a27feab6
chameleon05119 <chameleon05119@gmail.com>
2023-12-31 16:47:13 +0900

    リアクティブな変数についての勉強

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// 時刻の初期化
+const now = new Date()
+const nowStr = now.toLocaleTimeString()
+let timeStr = nowStr
+const timeStrRef = ref(nowStr)
+
+// 新しい時刻に変換する関数
+function changeTime(): void {
+  const newTime = new Date()
+  const newTimeStr = newTime.toLocaleTimeString()
+  timeStr = newTimeStr
+  timeStrRef.value = newTimeStr
+}
+
+// 1秒ごとに実行
+setInterval(changeTime, 1000)
+</script>
+
+<template>
+  <p>現在時刻：{{ timeStr }}</p>
+  <p>現在時刻(ref)：{{ timeStrRef }}</p>
+</template>
